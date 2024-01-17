cf2aa3bda1659be4eeb8907b044308a983557204
CesarIzquierdo <cesarizquierdo100@gmail.com>
2024-01-17 11:22:37 -0600

    se agreggo el panel de administracion, filtros y busqueda de las tablas

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from gestionPedidos.models import Clientes, Articulos, Pedidos
 # Register your models here.
+
+class ClientesAdmin(admin.ModelAdmin):
+    list_display=("nombre", "direccion","telefono")
+    search_fields=("nombre", "telefono")
+    
+class ArticulosAdmin(admin.ModelAdmin):
+    list_filter=("seccion",)
+    
+    
+class PedidosAdmin(admin.ModelAdmin):
+    list_display=("numero","facha")
+    list_filter=("facha",)
+    date_hierarchy="facha"
+
+admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidosAdmin)
+
